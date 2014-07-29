aea4f4289e7806104995db13075323996372ab8e
Alfredo David Albor Millan <adam-1009@hotmail.com>
2014-07-29 14:18:57 -0500

    Create poten.py

---
+++ poten.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+def funcion(c, d):
+        res = c**d
+        return res
+
+c = int(input("Dame la base: "))
+d = int(input("Dame el exponente: "))
+print c, "elevado a la potencia", d, "es:", funcion(c, d)
