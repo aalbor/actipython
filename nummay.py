e393340f85feb44009e4439732ce3d62990215a4
Alfredo David Albor Millan <adam-1009@hotmail.com>
2014-07-29 14:15:37 -0500

    Create nummay.py

---
+++ nummay.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+def funcion():
+        if a>b:
+                return  True
+        elif a<b:
+                return  False
+        else:
+                return "Los numeros son iguales"
+
+a = int(input("Escriba un numero: "))
+b = int(input("Escriba otro numero: "))
+x = funcion()
+
+if x == True:
+        print a, "Es mayor que", b
+elif x == False:
+        print b, "Es mayor que", a
+else:
+        print x
