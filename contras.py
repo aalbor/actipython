aa364edc78286855e6aad538575b0ab878a67c2a
Alfredo David Albor Millan <adam-1009@hotmail.com>
2014-07-29 14:10:35 -0500

    Create contras.py

---
+++ contras.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import re
+def cont():
+        pat = "^.*(?=.[^\s]{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!.@?#$%^&+=,:;]).*$"
+        resul = re.findall(pat, contra)
+                if (resul):
+                        return "contrasena valida"
+                else:
+                        return "contrasena no valida"
+
+contra = raw_input("Dame la contrasena que deseas tener: ")
+while True:
+        print contra
+        n = raw_input("Deseas que esta sea tu contrasena, escribe 'SI'. Si deseas cambiarla, escribe 'NO' ")
+        if n.strip() == 'SI':
+                print contra,'*',cont(),'*'
+                break
+        if n.strip() == 'NO':
+                contra = raw_input("Dame la contrasena que deseas tener: ")
