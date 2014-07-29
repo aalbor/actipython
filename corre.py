ae3d217925926262a70eb36e7d45a67ce7127ed7
Alfredo David Albor Millan <adam-1009@hotmail.com>
2014-07-29 14:11:23 -0500

    Create corre.py

---
+++ corre.py
@@ -0,0 +1,14 @@
+import re
+
+def correo():
+        f = open('correos.txt','r')
+        str = f.readlines()
+        for l in str:
+                if re.match('^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{2,4}$',str.lower()):
+                        return "Correo correcto"
+                else:
+                        return "Correo incorrecto"
+        f.close()
+        return str
+
+print correo()
